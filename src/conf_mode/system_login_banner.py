cf416328df2178282985a0bbc9137e9df0d73260
Viacheslav Hletenko <v.gletenko@vyos.io>
2024-10-17 10:40:00 +0000

    T6712: Replace motd warning to techpreview_warning

--- src/conf_mode/system_login_banner.py
+++ src/conf_mode/system_login_banner.py
@@ -28,7 +28,7 @@ airbag.enable()
 PRELOGIN_FILE = r'/etc/issue'
 PRELOGIN_NET_FILE = r'/etc/issue.net'
 POSTLOGIN_FILE = r'/etc/motd'
-POSTLOGIN_VYOS_FILE = r'/run/motd.d/01-vyos-nonproduction'
+POSTLOGIN_VYOS_FILE = r'/run/motd.d/01-vyos-techpreview_warning'
 
 default_config_data = {
     'issue': 'Welcome to VyOS - \\n \\l\n\n',
@@ -95,7 +95,7 @@ def apply(banner):
         render(POSTLOGIN_FILE, 'login/default_motd.j2', banner,
             permission=0o644, user='root', group='root')
 
-    render(POSTLOGIN_VYOS_FILE, 'login/motd_vyos_nonproduction.j2', banner,
+    render(POSTLOGIN_VYOS_FILE, 'login/techpreview_warning.j2', banner,
         permission=0o644, user='root', group='root')
 
     return None
